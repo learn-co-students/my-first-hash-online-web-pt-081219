15137a63d9846639aa8ae6c4e5205adfca80fee8
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-17 02:26:39 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,30 +1,33 @@
 def my_hash
+
   hash = {"dog" => "Pluto", "cat" => "Maru"}
 end
-def shipping_manifest 
+def shipping_manifest
   the_manifest = {"oil paintings" => 3, "porcelain vases" => 2, "whale bone corsets" => 5}
-end 
-            
-def retrieval               
+end
+
+def retrieval
   shipping_manifest = {
     "whale bone corsets"=> 5,
     "porcelain vases" => 2,
     "oil paintings" => 3
   }
-  
+
   shipping_manifest["oil paintings"]
 end
 
-def adding 
+def adding
   shipping_manifest = {
     "whale bone corsets"=> 5,
     "porcelain vases" => 2,
     "oil paintings" => 3
   }
-  
+
   shipping_manifest["muskets"] = 2
- 
+
   shipping_manifest["gun powder"] = 4
-  
+
   shipping_manifest
+
+  # hash = {"key1" => "value1"}
 end
